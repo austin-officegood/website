e227b8dc41742192ca7a25614de4386e3de2a056
austin-officegood <76885963+austin-officegood@users.noreply.github.com>
2021-01-20 17:13:45 -0800

    Adding Brand Images Section

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
+$(window).bind('scroll', function() {
+     if ($(window).scrollTop() > 100) {
+         $('#topInfoBar').hide();
+     }
+     else {
+         $('#topInfoBar').show();
+     }
+});
